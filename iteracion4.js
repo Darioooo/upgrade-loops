090bc0ac28b420fd9de2f67d9747291b80030e1c
Darioooo <darionietosastre@gmail.com>
2023-10-22 17:48:29 +0200

    iteracion#4

---
+++ iteracion4.js
@@ -0,0 +1,13 @@
+/* EJERCICIO 4: (NOTION)
+Usa un **for...in** para imprimir por consola los datos del alienígena.. Puedes usar este objeto:*/
+
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+console.log('Ejercicio 4:')
+for (let datoAlien in alien){
+    console.log(datoAlien, alien[datoAlien]);
+}
